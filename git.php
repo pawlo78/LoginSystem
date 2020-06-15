fcbd7c500e65c61e60313a4ad524df05e9308be0
pawel wantola <pawlo78@poczta.fm>
2020-06-15 22:14:32 +0200

    description of git

---
+++ git.php
@@ -0,0 +1,15 @@
+<?php
+
+    //time = 18.00
+    //git fetch --all
+    //git reset --hard origin/master
+
+    //git clone https 
+    //git add . or git add -u
+    //git commit -m "txt"
+    //git pull origin master
+    //git push origin master
+
+
+
+?>
